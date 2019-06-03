3f0809229ed46afce20ae48993849bc8174c2abe
Andrea Capriotti <a.capriotti@cineca.it>
2019-06-03 19:24:50 +0200

    Set IPC mode host.

--- docker-firefox.sh
+++ docker-firefox.sh
@@ -11,13 +11,12 @@ xhost +local:
 
 USER_UID=$(id -u)
 USER_GID=$(id -g)
-
 if ! container_exists storage ; then
 	docker run --env=USER_UID=$USER_UID --env=USER_GID=$USER_GID --name storage capriott/docker-storage
 fi
 
 if ! container_exists firefox ; then
-	exec docker run -d --env=USER_UID=$USER_UID --env=USER_GID=$USER_GID --name firefox --volumes-from storage --env DISPLAY="${DISPLAY}" --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro --volume=/run/user/1000/pulse:/run/pulse:ro capriott/docker-firefox
+	exec docker run -d --env=USER_UID=$USER_UID --env=USER_GID=$USER_GID --name firefox --ipc host  --volumes-from storage --env DISPLAY="${DISPLAY}" --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro --volume=/run/user/1000/pulse:/run/pulse:ro capriott/docker-firefox
 fi
 
 exec docker start firefox
